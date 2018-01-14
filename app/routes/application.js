ee172ee934608d9bacb5d6e0858a73f128c9e04d
Dohyub Lee <dohyub@9ple.com>
2018-01-14 19:11:21 +0900

    test 완료

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,6 @@
 import Route from '@ember/routing/route';
 import { action } from 'ember-decorators/object';
-import config from 'reflex/config/environment';
+import config from 'ember-set/config/environment';
 import { service } from 'ember-decorators/service';
 
 export default Route.extend({
